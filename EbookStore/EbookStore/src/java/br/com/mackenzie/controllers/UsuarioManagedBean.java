fbc8913caee3dae038085422fb5edaa8f5630133
luizoscar.lima <luizoscar.lima@gmail.com>
2013-11-23 14:01:04 -0200

    [add] login usuário.

---
+++ EbookStore/EbookStore/src/java/br/com/mackenzie/controllers/UsuarioManagedBean.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mackenzie.controllers;
+
+import br.com.mackenzie.dao.UsuarioDAO;
+import br.com.mackenzie.dominio.Usuario;
+import br.com.mackenzie.dominio.enumeracoes.Perfil;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author luizoscar.lima
+ */
+@ManagedBean
+@SessionScoped
+public class UsuarioManagedBean {
+
+    private Usuario usuarioFormulario;
+    private Usuario usuarioLogado;
+    @EJB
+    private UsuarioDAO usuarioDAO;
+
+    public UsuarioManagedBean() {
+        this.usuarioFormulario = new Usuario();
+    }
+
+    public Usuario getUsuarioFormulario() {
+        return usuarioFormulario;
+    }
+
+    public void setUsuarioFormulario(Usuario usuario) {
+        this.usuarioFormulario = usuario;
+    }
+
+    public Usuario getUsuarioLogado() {
+        return usuarioLogado;
+    }
+
+    public void setUsuarioLogado(Usuario usuarioLogado) {
+        this.usuarioLogado = usuarioLogado;
+    }
+    
+    public String entrar() {
+        String url = "index?faces-redirect=true";
+        this.usuarioLogado = this.usuarioDAO.obter(this.usuarioFormulario.getEmail(), this.usuarioFormulario.getSenha());
+        if (this.usuarioLogado == null) {
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Informe o usuário e senha corretamente" ,"Informe o usuário e senha corretamente" );
+            FacesContext.getCurrentInstance().addMessage(null, m);
+            url = "login";
+        }
+        
+        this.limparObjetoUsuario();
+        
+        return url;
+    }
+
+    public void sair() {
+    }
+
+    public void criar() {
+        usuarioFormulario.setPerfil(Perfil.Cliente);
+        this.usuarioDAO.inserir(usuarioFormulario);
+        this.limparObjetoUsuario();
+    }
+    
+    public void limparObjetoUsuario(){
+        this.setUsuarioFormulario(new Usuario());
+    }
+    
+    public String getLoginUsuario(){
+        String loginUsuario = "visitante";
+        if (this.usuarioLogado != null) {
+            loginUsuario = this.usuarioLogado.getEmail();
+        }
+        
+        return loginUsuario;
+    }
+    
+    public String urlAutenticado(){
+        String url = "login?faces-redirect=true";
+        if (usuarioLogado != null) {
+            url = "minhapagina?faces-redirect=true";
+        }
+        
+        return url;
+    }
+}
