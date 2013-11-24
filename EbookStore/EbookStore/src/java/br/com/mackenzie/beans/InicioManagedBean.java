245b20c5733f2bacdd1b9da4b91b7480904ba1f6
Graciela <Graciela@192.168.1.105>
2013-11-24 21:33:46 -0200

    [add] tamanho do carrinho na master page.

--- EbookStore/EbookStore/src/java/br/com/mackenzie/beans/InicioManagedBean.java
+++ EbookStore/EbookStore/src/java/br/com/mackenzie/beans/InicioManagedBean.java
@@ -1,6 +1,8 @@
 package br.com.mackenzie.beans;
 
+import br.com.mackenzie.dao.GeneroDAO;
 import br.com.mackenzie.dao.LivroDAO;
+import br.com.mackenzie.dominio.Genero;
 import br.com.mackenzie.dominio.Livro;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -12,6 +14,9 @@ public class InicioManagedBean {
 
     @EJB
     private LivroDAO livroDAO;
+    @EJB
+    private GeneroDAO generoDAO;
+    
     private String nomeAutor;
     
     public InicioManagedBean() {
@@ -19,16 +24,35 @@ public class InicioManagedBean {
     }
     
     public void inserir(){
+        
+        Genero genero = new Genero();
+        genero.setNome("jhahuajajja");
+        generoDAO.inserir(genero);
+
+                
+        Genero genero2 = new Genero();
+        genero2.setNome("skjandhauhsdoai");
+        generoDAO.inserir(genero2);
+        
         Livro livro = new Livro("Chico bento", 100, "Ingles", "um livro muito legal do chico bento", 29.90, 30);
+        livro.setGenero(genero);
         livro.setCapa("carrinho.png");
+        
         Livro livro1 = new Livro("Turma da monica", 100, "Ingles", "um livro muito legal da turma da monica", 29.90, 2);
         livro1.setCapa("carrinho.png");
+        livro1.setGenero(genero2);
+
         Livro livro2 = new Livro("Os coisas", 100, "Ingles", "um livro muito legal dos coisas", 29.90, 100);
         livro2.setCapa("carrinho.png");
+        livro2.setGenero(genero);
+        
         Livro livro3 = new Livro("Xando o Filme", 100, "Ingles", "um livro muito legal xando o filme", 29.90, 0);        
         livro3.setCapa("carrinho.png");
+        livro3.setGenero(genero2);
+        
         Livro livro4 = new Livro("Vc eh doida demais", 100, "Ingles", "um livro muito legal vc é doida demais", 29.90, 0);
         livro4.setCapa("carrinho.png");
+        livro4.setGenero(genero);
         
         livroDAO.inserir(livro);
         livroDAO.inserir(livro1);
